1c1b81a60bec3021cd48d4485fb397007605773e
jubcow <33065107+jubcow@users.noreply.github.com>
2020-02-25 16:43:29 -0500

    idk

--- assign1/Ahuh.py
+++ assign1/Ahuh.py
@@ -9,12 +9,17 @@ p = [[1,2,4],[5,6],[2,3,4]]
 def flatten(listBoi):
     result = []
     for x in listBoi:
-        if len(x) > 1 and x:
-            for i in x:
-                result.append(i)
+        if isinstance(x, list): 
+            if len(x) > 1:
+                for i in x:
+                    result.append(i)
+            else:
+                if x:
+                    result.append(x)
         else:
             if x:
                 result.append(x)
     return result
 
 print(flatten(p))
+print(flatten([[1,2,3], [[4],[5]], 6,7,8]))
