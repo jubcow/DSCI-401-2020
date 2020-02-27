3ea0ab460b77154adbd54524dedc8d81209144a5
jubcow <33065107+jubcow@users.noreply.github.com>
2020-02-27 15:49:54 -0500

    yuh

--- assign1/Ahuh.py
+++ assign1/Ahuh.py
@@ -19,43 +19,39 @@ and n^2 – 1 in the ending corner, which spirals in the clockwise direction. He
 n = 8 ending in corner 2:
 """
 
-def flatten(listBoi):
-    result = []
-    for x in listBoi:
-        if isinstance(x, list): 
-            if len(x) > 1:
-                for i in x:
-                    result.append(i)
-            else:
-                if x:
-                    result.append(x)
+def flatten(listBoi, memory=[]):
+    for x in listBoi:           #for every element in the list
+        if isinstance(x, list): #If it is another list, then:
+            flatten(x,memory)   #We repeat the last few steps in that list
         else:
-            if x:
-                result.append(x)
-    return result
+            memory.append(x)    #Other wise, we add it to our memory 
+                
+    return memory
 
-print("Flatten of example: ", flatten([[1,2,3], [[4],[5]], 6,7,8]))
+print("Flatten on example: ", flatten([[1,2,3], [[4],[5]], 6,7,8]))
 ############################################################################
 
 from itertools import chain, combinations
 # https://stackoverflow.com/questions/1482308/how-to-get-all-subsets-of-a-set-powerset
 #TODO own
-def powSet(setBoi): #maybe a dict idk   
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
-    s = list(setBoi)
-    return list(chain.from_iterable(combinations(s, r) for r in range(len(s)+1)))
+def powSet(setBoi):  
+    return list(chain.from_iterable(combinations(setBoi, r) for r in range(len(setBoi)+1)))
     
-print("Powerset of 1 2 3: ", powSet([1,2,3])) 
-
+print("\nPowerset of 1 2 3: ", powSet([1,2,3])) 
 ############################################################################
-#TODO
+
+#https://www.geeksforgeeks.org/permutation-and-combination-in-python/
+#TODO own
+from itertools import permutations
+
 def allPerms(listBoi):
-    if len(listBoi) == 0:
-        return []
-    if len(listBoi) == 1:
-        return listBoi
+    result = list(permutations(listBoi))
+    
+    return(result)
 
+print("\nPermutations of 1 2 3: ", allPerms([1,2,3]))
 ############################################################################
+
 import numpy as np
 """
 use a list of lists
